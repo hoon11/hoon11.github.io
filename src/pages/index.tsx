2fed3530d5b28b5aeeec785edc5b01e54ad788f2
jh <jhoonen@gmail.com>
2022-10-29 17:58:49 +0900

    基本構成

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Container from '@mui/material/Container';
+import Footer from 'src/components/Footer';
+import Header from 'src/components/Header';
+
+export default function Home() {
+  return (
+    <Container maxWidth="lg">
+
+    </Container>
+    <Footer
+    title="Footer"
+    description="Something here to give the footer a purpose!"
+  />
+  )
+}
